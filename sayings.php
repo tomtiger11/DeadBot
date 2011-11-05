5241a0fb4016cc077f760e9b5b4975992d8aabe2
Dead-i <ethan.waite@btinternet.com>
2011-11-05 08:30:15 +0000

    Adding a new saying

--- sayings.php
+++ sayings.php
@@ -6,4 +6,10 @@ switch ($entirecommandraw) {
 		fputs($socket, "PRIVMSG ".$ex[2]." ".$recipient.": I know it gets boring after a while, but I'm kinda forced to unless I feel like throwing an error.\n");
 		break;
 		
+	case 'Be good!':
+		$random = rand(1, 2);
+		if ($random == 1) fputs($socket, "PRIVMSG ".$ex[2]." :I'll try.\n");
+		if ($random == 2) fputs($socket, "PRIVMSG ".$ex[2]." :Fat chance!\n");
+		break;
+		
 }
