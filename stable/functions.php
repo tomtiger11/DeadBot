3ca7969d9e3e6e580d5b34fd613689c928f11f36
Dead-i <ethan.waite@btinternet.com>
2011-11-14 20:55:40 +0000

    Uploading stable foundations

---
+++ stable/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+########################################
+## DeadBot IRC Bot Configuration File ##
+##### Important core bot functions #####
+########################################
+
+function raw($message) {
+	fputs($socket, $message."\n");
+}
+
+function sync() {
+	$adminfile = file_get_contents('./admins.txt');
+	$hostmasks = file_get_contents('./hostmasks.txt');
+}
+
+function startup() {
+	set_time_limit(0);
+	$startseconds = time();
+	$current = date('ymdHis');
+	$socket = fsockopen($network, 6667);
+	raw("USER ".$nick." ".$name." CM :".$nick);
+	raw("NICK ".$nick);
+	raw("JOIN ".$channel1);
+	raw("JOIN ".$channel2);
+	sync();
+}
---
+++ stable/index.php
@@ -0,0 +1,26 @@
+<?php
+
+########################################
+##### DeadBot IRC Bot - 1.0 STABLE #####
+#### www.github.com/Dead-i/DeadBot #####
+## See README.md for info and licenseÊ##
+########################################
+
+// Include the configuration file with important information about the bot
+require 'config.php';
+
+// Include the functions file to give accessibility for core files
+require 'functions.php';
+
+// Run the script startup commands
+startup();
+
+// Force an endless while
+while(1) {
+
+	while($data = fgets($socket, 522)) {
+		
+		
+	}
+	
+}
