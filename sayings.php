2c1453ee02fe8a3ade0100714966617ba25c9acd
Dead-i <ethan.waite@btinternet.com>
2011-11-16 20:36:10 +0000

    DeadBot Beta will die if botanic shoots him; new saying

--- sayings.php
+++ sayings.php
@@ -1,15 +1,23 @@
 <?php
-switch ($entirecommandraw) {
+switch (strtolower($entirecommandraw)) {
 	
-	case 'What are you doing?':
+	case 'what are you doing?':
 		fputs($socket, "PRIVMSG ".$ex[2]." ".$recipient.": I just sit here all day responding to you guys while being prodded by my master, Dead-i.\n");
 		fputs($socket, "PRIVMSG ".$ex[2]." ".$recipient.": I know it gets boring after a while, but I'm kinda forced to unless I feel like throwing an error.\n");
 		break;
 		
-	case 'Be good!':
+	case 'be good!':
 		$random = rand(1, 2);
 		if ($random == 1) fputs($socket, "PRIVMSG ".$ex[2]." :I'll try.\n");
 		if ($random == 2) fputs($socket, "PRIVMSG ".$ex[2]." :Fat chance!\n");
+		break;
+		
+	case 'deadbot: bang!':
+		fputs($socket, "PRIVMSG ".$ex[2]." :Aaaahhh!\n");
+		fputs($socket, "PART ".$ex[2]."\n");
+		sleep (6);
+		fputs($socket, "JOIN ".$ex[2]."\n");
+		fputs($socket, "PRIVMSG ".$ex[2]." :Healed! :P\n");
 		break;
 		
 }
